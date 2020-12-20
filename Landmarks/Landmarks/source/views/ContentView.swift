dc247c897c403c21b699727803d5682329f0b4f1
Dustin Pfannenstiel <dmp@hey.com>
2020-12-20 14:11:26 -0600

    Completed 'Building List and Navigation'

---
+++ Landmarks/Landmarks/source/views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Dustin Pfannenstiel on 12/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
