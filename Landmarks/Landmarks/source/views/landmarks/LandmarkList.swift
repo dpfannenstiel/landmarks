dafb508692e0ed4e89d74e02cd6974212592f2d5
Dustin Pfannenstiel <dmp@hey.com>
2020-12-20 16:05:01 -0600

    Completed 'Handling User Input'

---
+++ Landmarks/Landmarks/source/views/landmarks/LandmarkList.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Dustin Pfannenstiel on 12/20/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        //        ForEach(["iPhone SE (2nd generation)", "iPhone 11"], id: \.self) { deviceName in
+        LandmarkList()
+            .environmentObject(ModelData())
+        //                .previewDevice(PreviewDevice(rawValue: deviceName))
+        //                .previewDisplayName(deviceName)
+        //        }
+    }
+}
